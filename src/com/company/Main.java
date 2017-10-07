22eabf95cb5c82c6a41ea2c269738254c054c91f
lukasz <xpack321@gmail.com>
2017-10-07 11:41:35 +0200

    Lab1 - Gra w odgadywanie wylosowanej cyfry na kostce do gry

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Random cube = new Random();
+        Scanner odczyt = new Scanner(System.in);
+        int randomNumber;
+        int userNumber;
+
+        do {
+            randomNumber = cube.nextInt(6) + 1;
+            System.out.print("Podaj liczbe: ");
+            userNumber = odczyt.nextInt();
+
+            if (randomNumber == userNumber) {
+                System.out.println("Bingo!!!");
+                break;
+            } else {
+                System.out.println("Zle. Wylosowana liczba to: " + randomNumber);
+            }
+        } while (true);
     }
 }
